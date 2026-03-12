33e2f6cc41c64c336d7d82d79f7462e68a78862a
Tannisegcg <strowbjjin@gmail.com>
2026-03-12 16:03:39 +0800

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-if [! -f staff.txt]
+if [ ! -f staff.txt ]
 	then
 	echo "Staff file does not exist.
 Creating staff.txt…"
-	touch staff.txt "Requisition_ID:Staff_ID:Applicant_Name:Faculty:Contact_Number:Email"
-if [! -f requisition.txt]
+	echo "Requisition_ID:Staff_ID:Applicant_Name:Faculty:Contact_Number:Email" >> staff.txt
+fi
+
+if [ ! -f requisition.tx t]
 	then
 	echo "Book requistion file does not exist.
 Creating requisition.txt…"
-	touch requisition.txt "Requisition_ID:Requisition_Date:Author:Title:ISBN:Publisher:Year:Edition:Source:Price:Branch:Status"
+	echo "Requisition_ID:Requisition_Date:Author:Title:ISBN:Publisher:Year:Edition:Source:Price:Branch:Status" >> requisition.txt
+fi
 
+menuStart="Book Requisition Management System
+\nA – Add New Book Requisition Details
+\nU – Update Book Requisition Details
+\nD – Delete Book Requisition Details
+\nS – Search Book Requisition Details
+\nR – Sort Book Requisitions
+\n
+\nQ – Exit from Program
+\n
+\nPlease select a choice: "
 
-menuStart = "Book Requisition Management System
-A – Add New Book Requisition Details
-U – Update Book Requisition Details
-D – Delete Book Requisition Details
-S – Search Book Requisition Details
-R – Sort Book Requisitions
-
-Q – Exit from Program
-
-Please select a choice: "
-
-echo -n $menuStart
+printf "%s" "$menuStart"
 read choiceStart
 case "$choiceStart" in
 a|A)
@@ -68,3 +70,5 @@ Author: "
 	read inputBookLocation
 	echo -n "Status (Approved/Pending/Disapproved): "
 	read inputBookStatus
+
+esac
